95b9c7c5b6fa5654a5794faab3c007844f466f59
trexyt25 <ttrexyt51@gmail.com>
2025-07-15 15:59:38 -0700

    Agregar configuración de puerto para Render

--- app.py
+++ app.py
@@ -48,3 +48,7 @@ if __name__ == "__main__":
 @app.before_first_request
 def crear_tabla():
     db.create_all()
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
